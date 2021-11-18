c0c465cc0ca1e0e8facbd342caa28613b4d18808
lienko <larigdtk@gmail.com>
2021-11-18 20:11:56 -0300

    partial audiences instructions included

--- core/instructions/audiences.go
+++ core/instructions/audiences.go
@@ -23,8 +23,8 @@ import (
 	"github.com/Aereum/aereum/core/crypto"
 )
 
-
-type CreateAudience {
+// Create a new audience
+type CreateAudience struct {
 	Audience		[]byte	
 	Sumission		[]byte
 	Moderation		[]byte
@@ -35,15 +35,108 @@ type CreateAudience {
 	Description		string
 }
 
+func (s *CreateAudience) Serialize() []byte {
+	bytes := make([]byte, 0)
+	PutByteArray(s.Audience, &bytes)
+	PutByteArray(s.Submission, &bytes)
+	PutByteArray(s.Moderation, &bytes)
+	PutByteArray(s.AudienceKey, &bytes)
+	PutByteArray(s.SubmissionKey, &bytes)
+	PutByteArray(s.ModerationKey, &bytes)
+	bytes = append(bytes, s.Flag)
+	PutString(s.Description)
+	return bytes
+}
 
-type JoinAudience {
-	Version 		byte
-	MessageType		byte
-	
+func ParseCreateAudience(data []byte) *CreateAudience {
+    p := CreateAudience{}
+    position := 0
+    p.Audience, position = ParseByteArray(data, position)
+    if _, err := crypto.PublicKeyFromBytes(p.Audience); err != nil  {
+        return nil
+    }
+    p.Submission, position = ParseByteArray(data, position)
+    if _, err := crypto.PublicKeyFromBytes(p.Submission); err != nil  {
+        return nil
+    }
+    p.Moderation, position = ParseByteArray(data, position)
+    if _, err := crypto.PublicKeyFromBytes(p.Moderation); err != nil  {
+        return nil
+    }
+    p.AudienceKey, position = ParseByteArray(data, position)
+    p.SubmissionKey, position = ParseByteArray(data, position)
+    p.ModerationKey, position = ParseByteArray(data, position)
+    if position >= len(data) {
+        return nil
+    }
+    p.Flag = data[position]
+    position += 1
+    p.Description, position = ParseString(data, position)
+    if position == len(data) {
+        return &p
+    }
+    return nil
+}
+
+// Join request for an existing audience
+type JoinAudiece struct {
 	Audience		[]byte
-
 	Presentation	string
+}
+
+func (s *JoinAudiece) Serialize() []byte {
+	bytes := make([]byte, 0)
+	PutByteArray(s.Audience, &bytes)
+	PutString(s.Presentation, &bytes)
+	return bytes
+}
+
+func ParseJoinAudience(data []byte) *JoinAudience {
+	p := JoinAudience{}
+	position := 0
+	p.Audience, position = ParseByteArray(data, position)
+	if _, err := crypto.PublicKeyFromBytes(p.Audience); err != nil {
+		return nil
+	}
+	p.Presentation, position = ParseString(data, position)
+	if position == len(data) {
+        return &p
+    }
+    return nil
+}
 
 
+// Accept an audience's join request
+type AcceptJoinAudience struct {
+	Audience	[]byte
+	Member		[]byte
+	Read		[]byte
+	Submit		[]byte
+	Moderate	[]byte
+}
+
+func (s *AcceptJoinAudience) Serialize() []byte {
+	bytes := make([]byte, 0)
+	PutByteArray(s.Audience, &bytes)
+	PutByteArray(s.Member, &bytes)
+	PutByteArray(s.Read, &bytes)
+	PutByteArray(s.Submit, &bytes)
+	PutByteArray(s.Moderate, &bytes)
+	return bytes
+}
+
+func ParseAcceptJoinAudience(data []byte) *AcceptJoinAudience {
+	p := AcceptJoinAudiece{}
+	position := 0
+	p.Audience, position = ParseByteArray(data, position)
+	p.Audience, position = ParseByteArray(data, position)
+	if _, err := crypto.PublicKeyFromBytes(s.Audience); err != nil {
+		return nil
+	}
+	p.Presentation, position = ParseString(data, position)
+	if position == len(data) {
+        return &p
+    }
+    return nil
 
 }
