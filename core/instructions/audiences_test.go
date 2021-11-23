56c91a4310fbd3a91ebce6eb5e6462f649d86eca
Ruben M. Damião <rmdamiao@gmail.com>
2021-11-23 10:16:30 -0300

    transfer test

--- core/instructions/audiences_test.go
+++ core/instructions/audiences_test.go
@@ -1,23 +1,17 @@
 package instructions
 
-import (
-	"reflect"
-	"testing"
-
-	"github.com/Aereum/aereum/core/crypto"
-)
-
+/*
 func TestCreateAudience(t *testing.T) {
-	
+
 	// Creating 3 public-private keys for audience identification, submission and moderation
 	public_audience, private_audience := crypto.RandomAsymetricKey()
 	public_submission, private_submission := crypto.RandomAsymetricKey()
 	public_moderation, private_moderation := crypto.RandomAsymetricKey()
-	
+
 	cipher_key_audience := crypto.NewCipherKey()
 	cipher_key_submission := crypto.NewCipherKey()
 	cipher_key_moderation := crypto.NewCipherKey()
-	
+
 	cipher_submission := crypto.CipherNonceFromKey(cipher_key_submission)
 	cipher_moderation := crypto.CipherNonceFromKey(cipher_key_moderation)
 
@@ -27,7 +21,7 @@ func TestCreateAudience(t *testing.T) {
 		Audience:    public_audience.ToBytes(),
 		Submission:  public_submission.ToBytes(),
 		Moderation:  public_moderation.ToBytes(),
-		AudienceKey: 
+		AudienceKey:
 
 	}
 	bytes := message.Serialize()
@@ -56,3 +50,4 @@ func Test(t *testing.T) {
 		t.Error("Parse and Serialization not working for GrantPowerOfAttorney messages.")
 	}
 }
+*/
