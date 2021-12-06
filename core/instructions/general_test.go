524e72fa24bda656c8ca63b81e6a6aab574e2a0a
Ruben M. Damião <rmdamiao@gmail.com>
2021-12-06 15:57:58 -0300

    general test details

--- core/instructions/general_test.go
+++ core/instructions/general_test.go
@@ -20,8 +20,16 @@ func TestGeneral(t *testing.T) {
 	firstAuthor := &Author{token: &prvKey, wallet: &token}
 	var inst Instruction
 	inst = creator.NewJoinNetworkThirdParty(pubKey.ToBytes(), "First Member", jsonString1, 1, 1)
-	block.Incorporate(inst)
+	if block.Incorporate(inst) != true {
+		t.Error("could not add new member")
+	}
 	state.IncorporateBlock(block)
+	if !state.Members.Exists(crypto.Hasher(pubKey.ToBytes())) {
+		t.Error("state did not add new member")
+	}
+	if !state.Captions.Exists(crypto.Hasher([]byte("First Member"))) {
+		t.Error("state did not add new caption")
+	}
 	block = NewBlock(crypto.Hasher([]byte{}), 0, 2, token.PublicKey().ToBytes(), validator)
 	inst = firstAuthor.NewUpdateInfo(jsonString1_new, 12, 10)
 	block.Incorporate(inst)
